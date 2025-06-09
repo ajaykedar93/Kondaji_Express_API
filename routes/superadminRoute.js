17984c4f744c7146330670d5a89f21c0a275d8da
ajaykedar123 <ajaykedar3790@gmail.com>
2025-06-09 16:57:42 +0530

    Updated API logic and cleaned bcrypt

--- routes/superadminRoute.js
+++ routes/superadminRoute.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
